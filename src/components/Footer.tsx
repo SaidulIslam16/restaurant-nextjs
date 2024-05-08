77239f62f857c94feff2d6f0181a10872705a800
SaidulIslam16 <isaidulislamsayed@gmail.com>
2024-05-08 09:48:48 +0600

    footer added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='flex justify-between lg:p-10 p-5'>
+      <Link href='/' className='uppercase font-bold text-xl text-red-500'>Aperitivo</Link>
+      <p>2024 All Right Researsed</p>
+    </div>
   )
 }
