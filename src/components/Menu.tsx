465c92557eaefd579005addcaaae29fb9d22a278
SaidulIslam16 <isaidulislamsayed@gmail.com>
2024-05-08 08:12:12 +0600

    notification bar added

---
+++ src/components/Menu.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Menu() {
+  return (
+    <div>
+        <Image src='/public/open.png/' alt='' height={20} width={20}/>
+    </div>
+  )
+}
---
+++ src/components/Notification.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Notification() {
+  return (
+    <div className='h-12 bg-red-500 text-white text-sm md:text-base flex justify-center items-center text-center cursor-pointer px-4'>Free Delivery over $50 Order (All Over the Coountry)</div>
+  )
+}
