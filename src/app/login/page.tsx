62c4937ac6a908d8e0b1830597ee66a199e97f57
SaidulIslam16 <isaidulislamsayed@gmail.com>
2024-05-09 13:07:52 +0600

    login page added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 export default function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
+      {/* BOX */}
+      <div className="h-full md:h-[70%] w-full shadow-lg rounded-md flex flex-col md:flex-row lg:w-[60%]">
+        {/* IMAGE CONTAINER */}
+        <div className="relative h-1/3 w-full md:h-full md:w-1/2">
+          <Image src="/loginBg.png" alt="" fill className="object-cover" />
+        </div>
+        {/* FORM CONTAINER */}
+        <div className="p-8 flex flex-col gap-5 md:w-1/2">
+          <h1 className="uppercase text-2xl font-bold xl:text-4xl">Welcome</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, illo?
+          </p>
+          {/* Google Login Button */}
+          <button className="ring-1 ring-gray-300 p-4 flex gap-2 items-center text-lg rounded-md">
+            <Image
+              src="/google.png"
+              alt=""
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <span>Sign in with Google</span>
+          </button>
+          {/* Facebook Login Button */}
+          <button className="ring-1 ring-gray-300 p-4 flex gap-2 items-center text-lg rounded-md">
+            <Image
+              src="/facebook.png"
+              alt=""
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <span>Sign in with Facebook</span>
+          </button>
+          <p className="text-sm">
+            Have a Problem{" "}
+            <Link className="underline" href="/">
+              Contact us
+            </Link>{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,37 +1,43 @@
-import React from 'react'
-import Menu from './Menu'
-import Link from 'next/link'
-import CartIcon from './CartIcon';
-import Image from 'next/image';
+import React from "react";
+import Menu from "./Menu";
+import Link from "next/link";
+import CartIcon from "./CartIcon";
+import Image from "next/image";
 
 export default function Navbar() {
-
   const user = false;
 
   return (
-    <div className='h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase md:h-24 lg:px-10'>
+    <div className="h-12 text-red-500 p-4 flex justify-between items-center border-b-2 border-b-red-500 uppercase md:h-24 lg:px-10">
       {/* Left Link */}
-      <div className='hidden md:flex justify-between gap-8 items-center flex-1 md:justify-start'>
-        <Link href='/'>HomePage</Link>
-        <Link href='/menu'>Menu</Link>
-        <Link href='/'>Contact</Link>
+      <div className="hidden md:flex justify-between gap-8 items-center flex-1 md:justify-start">
+        <Link href="/">HomePage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
       </div>
       {/* Logo */}
-        <Link href='/' className='font-bold text-xl flex-1 text-center'>Aperitivo</Link>
+      <Link href="/" className="font-bold text-xl flex-1 text-center">
+        Aperitivo
+      </Link>
       {/* Mobile Menu */}
-      <div className='md:hidden'>
-        <Menu/>
+      <div className="md:hidden">
+        <Menu />
       </div>
       {/* Right Link */}
-      <div className='hidden md:flex justify-between gap-8 items-center flex-1 md:justify-end'>
-        <div className='bg-orange-500 flex text-white p-1 rounded-md cursor-pointer md:absolute top-2 r-2 lg:static'>
-          <Image src='/phone.png' alt='' height={12} width={18}/>
+      <div className="hidden md:flex justify-between gap-8 items-center flex-1 md:justify-end">
+        <div className="bg-orange-500 flex text-white p-1 rounded-md cursor-pointer md:absolute top-2 r-2 lg:static">
+          <Image src="/phone.png" alt="" height={12} width={18} />
           <span>1253 6693</span>
         </div>
-        {!user?<Link href='/'>Login</Link>:
-        <Link href='/menu'>Orders</Link>}
-        <Link href='/cart'><CartIcon/></Link>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/menu">Orders</Link>
+        )}
+        <Link href="/cart">
+          <CartIcon />
+        </Link>
       </div>
     </div>
-  )
+  );
 }
